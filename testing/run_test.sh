e8c856d1af9ca36ef068c97e57ef77819dcf9abc
Thomas E. Dickey <dickey@invisible-island.net>
1990-08-30 13:07:22 -0400

    snapshot of project "c_count", label t19900830

--- testing/run_test.sh
+++ testing/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: run_test.sh,v 3.0 1990/05/11 08:03:32 ste_cm Rel $
+# $Id: run_test.sh,v 5.0 1990/08/30 08:02:54 ste_cm Rel $
 #
 PROG=../bin/lincnt
 TMP=/tmp/lincnt$$
@@ -7,38 +7,41 @@ rm -f $TMP
 #
 cat <<EOF/
 **
-**	Count lines in test-files (which have both unbalanced quotes and
-**	"illegal" characters:
+**	Case 1:	Count lines in test-files (which have both unbalanced quotes and
+**		"illegal" characters:
 EOF/
 $PROG test[12].c >$TMP
-cat  $TMP
-diff $TMP normal.ref
+./show_diffs.sh $TMP normal.ref
 #
 cat <<EOF/
 **
-**	Suppressing unbalanced-quote:
+**	Case 2:	Suppressing unbalanced-quote:
 EOF/
 $PROG -qLEFT test[12].c >$TMP
-cat  $TMP
-diff $TMP quotes.ref
+./show_diffs.sh $TMP quotes.ref
 #
 cat <<EOF/
 **
-**	Counting by names given in standard-input:
+**	Case 3:	Counting by names given in standard-input:
 EOF/
 $PROG >$TMP <<EOF/
 test1.c
 test2.c
 EOF/
-cat  $TMP
-diff $TMP list.ref
+./show_diffs.sh $TMP list.ref
 #
 cat <<EOF/
 **
-**	Counting bulk text piped in standard-input:
+**	Case 4:	Counting bulk text piped in standard-input:
 EOF/
 cat test[12].c | $PROG -v - >$TMP
-cat  $TMP
-diff $TMP cat.ref
+./show_diffs.sh $TMP cat.ref
+#
+cat <<EOF/
+**
+**	Case 5:	Counting history-comments
+EOF/
+$PROG test3.c >$TMP
+./show_diffs.sh $TMP history.ref
 #
 rm -f $TMP
