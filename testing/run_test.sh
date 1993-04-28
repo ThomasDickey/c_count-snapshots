57c1fd6c645308b7b37a21939e7a92950a784568
Thomas E. Dickey <dickey@invisible-island.net>
1993-04-28 09:40:23 -0400

    snapshot of project "c_count", label t19930428

--- testing/run_test.sh
+++ testing/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: run_test.sh,v 6.0 1991/10/21 16:06:48 ste_cm Rel $
+# $Id: run_test.sh,v 7.0 1993/04/28 09:40:23 ste_cm Rel $
 #
 PROG=../bin/lincnt
 TMP=/tmp/lincnt$$
@@ -11,14 +11,14 @@ cat <<EOF/
 **		"illegal" characters:
 EOF/
 $PROG test[12].c >$TMP
-./show_diffs.sh $TMP normal.ref
+./showdiff.sh $TMP normal.ref
 #
 cat <<EOF/
 **
 **	Case 2:	Suppressing unbalanced-quote:
 EOF/
 $PROG -qLEFT test[12].c >$TMP
-./show_diffs.sh $TMP quotes.ref
+./showdiff.sh $TMP quotes.ref
 #
 cat <<EOF/
 **
@@ -28,35 +28,35 @@ $PROG >$TMP <<EOF/
 test1.c
 test2.c
 EOF/
-./show_diffs.sh $TMP list.ref
+./showdiff.sh $TMP list.ref
 #
 cat <<EOF/
 **
 **	Case 4:	Counting bulk text piped in standard-input:
 EOF/
 cat test[12].c | $PROG -v - >$TMP
-./show_diffs.sh $TMP cat.ref
+./showdiff.sh $TMP cat.ref
 #
 cat <<EOF/
 **
 **	Case 5:	Counting history-comments
 EOF/
 $PROG test3.c >$TMP
-./show_diffs.sh $TMP history.ref
+./showdiff.sh $TMP history.ref
 #
 cat <<EOF/
 **
 **	Case 6:	Display as a spreadsheet
 EOF/
 $PROG -t -qLEFT test[123].c >$TMP
-./show_diffs.sh $TMP table.ref
+./showdiff.sh $TMP table.ref
 #
 cat <<EOF/
 **
 **	Case 7:	Display as a spreadsheet (per-file)
 EOF/
 $PROG -pt -qLEFT test[123].c >$TMP
-./show_diffs.sh $TMP table_p.ref
+./showdiff.sh $TMP table_p.ref
 #
 #
 rm -f $TMP
